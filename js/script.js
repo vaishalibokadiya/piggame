62e746f24906c8cadce0e9169e093929934293d4
vaishalibokadiya <vaishali.bokadiya@gmail.com>
2021-04-11 12:32:13 +0530

    2nd commit

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ btnRoll.addEventListener("click", function () {
     const dice = Math.trunc(Math.random() * 6) + 1;
 
     diceEl.classList.remove("hidden");
-    diceEl.src = `../images/dice-${dice}.png`;
+    diceEl.src = `dice-${dice}.png`;
 
     if (dice !== 1) {
       currentScore += dice;
